864176b17c753fa695df418e39fa24924319ced3
hanneeul <haneulsky1217@naver.com>
2022-05-11 10:26:43 +0900

    Snake length 추가!

--- hello-git/src/com/kh/animal/Snake.java
+++ hello-git/src/com/kh/animal/Snake.java
@@ -2,12 +2,13 @@ package com.kh.animal;
 
 public class Snake {
 
+	private double length; // master에서 추가
 	private String name; // master에서 추가
 
 	public void move() {
 		System.out.println("스르르륵~");
 	}
-	
+
 	public void poisonAttack() {
 		System.out.println("독침 물기~");
 	}
